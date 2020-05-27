30a269141e2a1298ddeca6858ea905faf0df2995
Freddy <freddy.ari19@gmail.com>
2020-05-26 22:21:49 -0600

    Nuevo proyecto en 3 capas basado en SQL Server

---
+++ Proyecto Biblioteca/BAL/Capa_Logica/Cls_Rol.cs
@@ -0,0 +1,36 @@
+﻿using Capa_Datos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capa_Logica
+{
+    class Cls_Rol
+    {
+        public bool AgregarRol(string Nombre_Rol, string Descripcion)
+        {
+            try
+            {
+                int respuesta = 1;
+                bd_bibliotecaDataContext dc = new bd_bibliotecaDataContext();
+                respuesta = Convert.ToInt32(dc.AgregarRol(Nombre_Rol, Descripcion));
+
+                if (respuesta == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+    }
+}
---
+++ Proyecto Biblioteca/BAL/Capa_Presentacion/App_Start/BundleConfig.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace Capa_Presentacion.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/Js").Include(
+                "~/Content/plugins/jQuery/jquery-2.2.3.min.js",
+                "~/Content/bootstrap/js/bootstrap.min.js",
+                "~/Content/plugins/slimScroll/jquery.slimscroll.min.js",
+                "~/Content/plugins/fastclick/fastclick.js",
+                "~/Content/dist/js/app.min.js",
+                "~/Content/dist/js/demo.js",
+                "~/Content/plugins/datatables/jquery.dataTables.min.js",
+                "~/Content/plugins/datatables/dataTables.bootstrap.min.js",
+                "~/Content/plugins/slimScroll/jquery.slimscroll.min.js",
+                "~/Content/plugins/fastclick/fastclick.js"));
+
+            bundles.Add(new StyleBundle("~/Css").Include(
+                "~/Content/bootstrap/css/bootstrap.min.css",
+                "~/Content/dist/css/AdminLTE.min.css",
+                "~/Content/dist/css/skins/_all-skins.min.css",
+                "~/Content/plugins/datatables/dataTables.bootstrap.css"));
+
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
