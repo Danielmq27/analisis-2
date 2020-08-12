987ba28fe089d63b41b1cd0bc48369ab6dcce60b
Freddy <freddy.ari19@gmail.com>
2020-08-11 22:56:20 -0600

    Cambio de contraseña malo

--- Proyecto Biblioteca/BAL/Capa_Presentacion/Controllers/IniciarSesionController.cs
+++ Proyecto Biblioteca/BAL/Capa_Presentacion/Controllers/IniciarSesionController.cs
@@ -25,37 +25,80 @@ namespace Capa_Presentacion.Controllers
             try
             {
                 clsUsuario usuario = new clsUsuario();
-                var oUsuario = usuario.Login(Seguridad.Encriptar(correo), Seguridad.Encriptar(clave)).First();
-                if (oUsuario == null)
+                var oUsuario = usuario.Login(Seguridad.Encriptar(correo), Seguridad.Encriptar(clave));
+                if(oUsuario.Count > 0)
+                {
+                    var Usuario = oUsuario.First();
+                    if (Usuario.IdRol == 1)
+                    {
+                        Session["Usuario"] = Usuario;
+                        Session["nombreUsuario"] = Usuario.nombre + " " + Usuario.apellido1;
+                        Session["Rol"] = "Administrador";
+                        return RedirectToAction("Administrador", "Principal");
+                    }else if (Usuario.IdRol == 2)
+                    {
+                        Session["Usuario"] = Usuario;
+                        Session["nombreUsuario"] = Usuario.nombre + " " + Usuario.apellido1;
+                        Session["Rol"] = "Editar";
+                        return RedirectToAction("Editar", "Principal");
+                    } else
+                    {
+                        Session["Usuario"] = Usuario;
+                        Session["nombreUsuario"] = Usuario.nombre + " " + Usuario.apellido1;
+                        Session["Rol"] = "Consultar";
+                        return RedirectToAction("Consultar", "Principal");
+                    }
+                }
+                else
                 {
                     TempData["msg"] = "<script>alert('Correo o contraseña incorrectos');</script>";
                     return View("Index");
-                }
-                else if (oUsuario.IdRol == 1)
-                {
-                    Session["Usuario"] = oUsuario;
-                    Session["nombreUsuario"] = oUsuario.nombre + " " + oUsuario.apellido1;
-                    Session["Rol"] = "Administrador";
-                    return RedirectToAction("Administrador", "Principal");
-                } 
-                else if (oUsuario.IdRol == 2)
-                {
-                    Session["Usuario"] = oUsuario;
-                    Session["nombreUsuario"] = oUsuario.nombre + " " + oUsuario.apellido1;
-                    Session["Rol"] = "Editar";
-                    return RedirectToAction("Editar", "Principal");
-                } 
-                else
-                {
-                    Session["Usuario"] = oUsuario;
-                    Session["nombreUsuario"] = oUsuario.nombre + " " + oUsuario.apellido1;
-                    Session["Rol"] = "Consultar";
-                    return RedirectToAction("Consultar", "Principal");
                 }
             }
             catch (Exception ex)
             {
                 return Content("Ocurrio un error :(" + ex.Message);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Recuperar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Recuperar(string correo, string cedula)
+        {
+            using(var bd = new bibliotecaDataContext())
+            {
+                int cantidad = bd.Usuario.Where(p => p.email == Seguridad.Encriptar(correo) && p.cedula == cedula).Count();
+                if (cantidad == 0)
+                {
+                    TempData["msg"] = "<script>alert('Usuario no existe');</script>";
+                    return View("Recuperar");
+                }
+                else
+                {
+                    //Se recupera el id del Usuario
+                    Usuario oUsuario = bd.Usuario.Where(p => p.email == Seguridad.Encriptar(correo) && p.cedula == cedula).First();
+
+                    //Nueva clave
+                    Random ra = new Random();
+                    int n1 = ra.Next(0, 9);
+                    int n2 = ra.Next(0, 9);
+                    int n3 = ra.Next(0, 9);
+                    int n4 = ra.Next(0, 9);
+                    string nuevaContra = n1.ToString() + n2.ToString() + n3.ToString() + n4.ToString();
+
+                    //Encriptar la nueva clave
+                     string contraEncriptada = Seguridad.Encriptar(nuevaContra);
+
+                    oUsuario.clave = contraEncriptada;
+                    Correo.enviarCorreo(correo, "Recuperar Clave", "Se reseteo su clave , ahora su clave es :" + nuevaContra, " ");
+                    TempData["msg"] = "<script>alert('Se ha enviado su nueva contraseña exitosamente al correo!');</script>";
+                    return View("Index");
+                }
             }
         }
     }
